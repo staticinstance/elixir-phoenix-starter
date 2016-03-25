a78677b3f1038c6da056cbd2cc0f6b805fa7c185
Michael Bradley <mbradleyis@gmail.com>
2016-03-25 13:58:45 -0700

    added httppoison

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,6 +1,31 @@
+defmodule GitHub do
+  use HTTPoison.Base
+
+  @expected_fields ~w(
+    login id avatar_url gravatar_id url html_url followers_url
+    following_url gists_url starred_url subscriptions_url
+    organizations_url repos_url events_url received_events_url type
+    site_admin name company blog location email hireable bio
+    public_repos public_gists followers following created_at updated_at
+  )
+
+  def process_url(url) do
+    "https://api.github.com" <> url
+  end
+
+  def process_response_body(body) do
+    body
+    |> Poison.decode!
+    |> Dict.take(@expected_fields)
+    |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
+  end
+end
+
+
+
 defmodule HelloPhoenix.RoomChannel do
   use Phoenix.Channel
-
+  
   def join("rooms:lobby", _message, socket) do
     {:ok, socket}
   end
@@ -9,11 +34,14 @@ defmodule HelloPhoenix.RoomChannel do
   end
 
   def handle_in("new_msg", %{"body" => body}, socket) do
-    broadcast! socket, "new_msg", %{body: body}
+    GitHub.start
+    
+    broadcast! socket, "new_msg", %{body: GitHub.get!("/users/staticinstance").body[:public_repos]}
     {:noreply, socket}
   end
 
   def handle_out("new_msg", payload, socket) do
+
     push socket, "new_msg", payload
     {:noreply, socket}
   end
